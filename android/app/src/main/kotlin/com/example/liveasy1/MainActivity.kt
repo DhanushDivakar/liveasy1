d7090724a6f1088e62f7f46420d5d2e87d7fad58
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-04-25 20:45:59 +0530

    d

---
+++ android/app/src/main/kotlin/com/example/liveasy1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.liveasy1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page1.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class page1 extends StatefulWidget {
+  const page1({Key? key}) : super(key: key);
+
+  @override
+  _page1State createState() => _page1State();
+}
+
+class _page1State extends State<page1> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            child: Image.asset("images/img.png"),
+          )
+        ],
+      ),
+    );
+  }
+}
